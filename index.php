c4c1c2f091e08cb354c0dfc3be43fd03b4e9b01d
artab-sccc <64048501+artab-sccc@users.noreply.github.com>
2020-07-16 14:09:42 -0700

    backup upload
    7/16/20 upload of portal website

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Arthur's Portal Page for Seattle Central Community College</title>
+
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+</head>
+    
+<body>
+
+<main>
+<div id="wrapper">
+    
+    <h1>Arthur's Portal Page</h1>
+    
+    <div class="topnav" id="myTopnav">
+        <a href="index.php" class="active">Welcome</a>
+        <a href="big/index.php">Big</a>
+        <a href="aia.php">AIA</a>
+        <a href="flowchart.php">Flowchart</a>
+        <a href="fp/index.php">Final Project</a>
+        <a href="contactme.php">Contact Arthur</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div> <!-- end topnav -->
+
+    <h2>Summer 2020 IT 162 at Seattle Cental College</h2>
+    <img src="images/ohdeerpng.png" alt="oh deer">
+    
+    <p>Hello! Welcome to my portal page for Summer 2020 IT 162 class at Seattle Central College. </p>
+    
+    
+    <p>
+        My name is Arthur Tabadero and I am currently in the process of pursuing an Associates Degree in Programming at Seattle Central College. I have worked in the IT field for 5 years carrying titles from Technical Support Technician to Technical Program Manager. Upon my experiences, I <em>developed</em> a new interest in software and web development. Being a Seattle Central Alumni, I found my experience at Central very enriching, so I decided to start my self journey of pursing this new career field at an institution I can depend on. I look forward to achieving great knowledge in this field!
+    </p>
+    
+    <h2>About Me:</h2>
+    
+    <p>
+        When I'm not taking notes or scrambling to finish an assignment at the very last minute, I have various hobbies that keep me sane away from the workload. This includes listening to music, playing video games, traveling and best of all: sleeping. <strong>You know what they say, a good night sleep helps put you in a better mood and makes you smarter!</strong> While I agree with the former, the latter is a little questionable. Hah!
+    </p>
+    
+
+    
+<footer>
+    <ul>
+        <li>Copyright 2020 &copy;</li>
+        <li>All Right Reserved</li>
+        <li><a href="../index.html">Web design by Arthur</a></li>
+        <li><a href="https://validator.w3.org/check?uri=referer">Valid HTML</a></li>
+        <li><a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></li>
+    </ul> 
+</footer>
+    
+    
+</div> <!--end wrapper-->
+</main>
+
+
+    
+    
+</body>
+</html>
